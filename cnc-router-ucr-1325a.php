b34762ae629dddfe6f637f16c633eb4692a98b7d
mani <mani.cotginanalytics@gmail.com>
2023-07-04 10:46:20 +0530

    updated code

---
+++ cnc-router-ucr-1325a.php
@@ -0,0 +1,152 @@
+<?php include('header.php'); ?>
+
+  <div class="home-slider-area">
+    <div id="welcome-slide-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+    
+      <div class="carousel-inner" role="listbox"> 
+      
+        <div class="item active">
+          <div class="single-slide-item slide-5">
+            <div class="single-slide-item-table">
+              <div class="single-slide-item-table-cell">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <h2 style="text-transform:capitalize">CNC Router UCR 1325A</span></h2>
+                    
+                    <!-- <a class="slide-btn" href="javascript:void(0)">Contact Us</a>--></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        
+  </div>
+    </div>
+  </div>
+
+
+
+  <div class="home-aboutus-wrapper detail-page">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-12 head">
+                  <h2>UCR 1325A</h2>
+              </div>
+              <div class="paragraph">
+                  <p>Widely used for engraving, membrane door designing, acrylic carving, decorative panels, wave boards, grill boards etc.</p>
+              </div>
+          </div>
+      </div>
+  </div>
+
+  <div class="home-aboutus-wrapper detail-page">
+      <div class="container">
+        <div class="row">
+        <div class="col-md-12 text-center">
+          <div class="col-md-6 ">
+            <div class="about-text">
+              
+            <img src="images/Multi-Boring-63.png" alt=""> </div>
+        <a href="contact.html" class="btn">Contact Us</a> <a href="tel:919723468755" class="btn">Call Now</a>
+          </div>
+          <div class="col-md-6">
+            <div class="about-text">
+            <!-- <h3>Reciprocating Beam Saw Machine</h3><hr/> -->
+              
+            <table>
+    <tr>
+      <th>MODEL</th>
+      <th>UMB 21</th>
+      
+    </tr>
+    <tr>
+      <td><b>Distance between the bit</b> </td>
+      <td>3280 mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Horizontal head</b> </td>
+      <td>1</td>
+      
+    </tr>
+    <tr>
+      <td><b>Vertical head</b> </td>
+      <td>2</td>
+      
+    </tr>
+    <tr>
+      <td><b>Tilting Head</b> </td>
+      <td>1</td>
+      
+    </tr>
+    <tr>
+      <td><b>The quantity of spindle</b></td>
+      <td>63</td>
+      
+    </tr>
+    <tr>
+      <td><b>Maximum boring diameter</b></td>
+      <td>35mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Maximum panel thickness</b></td>
+      <td>60mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Maximum panel width</b></td>
+      <td>1800mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Minimum panel width</b></td>
+      <td>-</td>
+      
+    </tr>
+    <tr>
+      <td><b>Total power</b></td>
+      <td>4.5kw</td>
+      
+    </tr>
+  </table>
+
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="home-aboutus-wrapper detail-page">
+      <div class="container">
+        <div class="row">
+      
+          <div class="col-md-6">
+            <div class="about-text">
+        <h3>Salient Features </h3><hr/>
+          <ul class="list">
+            <li>The machine tool components choose the superior steel materials,they go through the highly and strong heat treatment.</li>
+            <li>The moving parts choose the high Grade anti-wear materials.</li>
+            <li>They are firm and durable.</li>
+            <li>Multi-drill has been elaborately made. It has multiple purposes with high speed and fine accuracy.</li>
+            <li>Equipped with one tilting head and two vertical head.</li>
+            <li>21 drilling bit in each head.</li>
+            <li>Mainly used for drilling in long board at one time.</li>
+          </ul>            
+            </div>
+          </div>
+          <div class="col-md-6 text-center">
+            <div class="about-text">
+        
+        <img src="images/UMB_63_1.jpg" alt=""> </div>
+        <a href="contact.html" class="btn">Contact Us</a> <a href="tel:919723468755" class="btn">Call Now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php include('footer.php'); ?>
---
+++ multi-boring-machine-umb-21.php
@@ -0,0 +1,131 @@
+<?php include('header.php'); ?>
+
+<div class="home-slider-area">
+  <div id="welcome-slide-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+   
+    <div class="carousel-inner" role="listbox"> 
+    
+      <div class="item active">
+        <div class="single-slide-item slide-5">
+          <div class="single-slide-item-table">
+            <div class="single-slide-item-table-cell">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-12">
+                    <h2 style="text-transform:capitalize">Multi-Boring UMB 21</span></h2>
+                   
+                   <!-- <a class="slide-btn" href="javascript:void(0)">Contact Us</a>--></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
+ </div>
+  </div>
+</div>
+
+
+
+<div class="home-aboutus-wrapper detail-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 head">
+                <h2>Perfect appearance for your Customer Satisfaction <br /> with absolutely Affordable Technology</h2>
+            </div>
+            <div class="paragraph">
+                <p>The multi boring machine for accurate and precise operation! Maximum precision when dowel boring with the Umaboy high quality drilling spindels positioned at 32 mm intervals, drills with a maximum diameter of 35 mm can be used, exact rows of holes and dowel bores are guaranteed. The solid steel table, stable fences, pneumatic workpiece clamps and ergonomical operating elements ensure Quality machines.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="home-aboutus-wrapper detail-page">
+    <div class="container">
+      <div class="row">
+       <div class="col-md-6 text-center">
+          <div class="about-text">
+          
+          <img src="images/MULTI-BORING_UMB_21.png" alt=""> </div>
+      <a href="contact.html" class="btn">Contact Us</a> <a href="tel:919723468755" class="btn">Call Now</a>
+        </div>
+        <div class="col-md-6">
+          <div class="about-text">
+          <!-- <h3>Computer Controlled Beam Saw Machine</h3><hr/> -->
+            
+            <table>
+    <tr>
+      <th>MODEL</th>
+      <th>UMB 21</th>
+      
+    </tr>
+    <tr>
+      <td><b>Distance between the bit</b> </td>
+      <td>3280 mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Horizontal head</b> </td>
+      <td>1</td>
+      
+    </tr>
+    <tr>
+      <td><b>Vertical head</b> </td>
+      <td>-</td>
+      
+    </tr>
+    <tr>
+      <td><b>Tilting Head</b> </td>
+      <td>1</td>
+      
+    </tr>
+    <tr>
+      <td><b>The quantity of spindle</b></td>
+      <td>21</td>
+      
+    </tr>
+    <tr>
+      <td><b>Maximum boring diameter</b></td>
+      <td>35mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Maximum panel thickness</b></td>
+      <td>60mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Maximum panel width</b></td>
+      <td>2400mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Minimum panel width</b></td>
+      <td>200mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Total power</b></td>
+      <td>1.5kw</td>
+      
+    </tr>
+  </table>
+  <!--<strong>Type</strong>: &nbsp;&nbsp;AD112 - 22FBA <br/><br/>
+  <strong>Color</strong>: &nbsp;&nbsp;White, Green, Red, Yellow, Blue<br/><br/>
+  <strong>Parameter</strong>: &nbsp;&nbsp;0-50A | 0-100A | 0-200A | 0-500A <br/><br/>
+  <strong>Outline Dimension</strong>:&nbsp;&nbsp;<img src="images/d1.jpg"><br/>-->
+  </p>
+  <p>
+    Note: Technical specifications may be changed without any prior notice. Product images are just for the reference.
+  </p>
+            
+           </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <?php include('footer.php'); ?>
