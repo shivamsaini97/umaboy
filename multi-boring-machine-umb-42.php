29732fb1c326d6d11fae71afd081bd5ae43e5298
Shivam Saini <shivam.cotginanalytics@gmail.com>
2023-07-03 23:41:20 -0700

    pull

--- multi-boring-machine-umb-42.php
+++ multi-boring-machine-umb-42.php
@@ -1,168 +1,144 @@
 <?php include('header.php'); ?>
-
-  <div class="home-slider-area">
-    <div id="welcome-slide-carousel" class="carousel slide carousel-fade" data-ride="carousel">
-    
-      <div class="carousel-inner" role="listbox"> 
-      
-        <div class="item active">
-          <div class="single-slide-item slide-5">
-            <div class="single-slide-item-table">
-              <div class="single-slide-item-table-cell">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-md-12">
-                      <h2 style="text-transform:capitalize">Multi-Boring UMB 42</span></h2>
-                    
-                    <!-- <a class="slide-btn" href="javascript:void(0)">Contact Us</a>--></div>
+<div class="home-slider-area">
+   <div id="welcome-slide-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+      <div class="carousel-inner" role="listbox">
+         <div class="item active">
+            <div class="single-slide-item slide-5">
+               <div class="single-slide-item-table">
+                  <div class="single-slide-item-table-cell">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-md-12">
+                              <h2 style="text-transform:capitalize">Multi-Boring UMB 42</span></h2>
+                              <!-- <a class="slide-btn" href="javascript:void(0)">Contact Us</a>-->
+                           </div>
+                        </div>
+                     </div>
                   </div>
-                </div>
-              </div>
+               </div>
             </div>
-          </div>
-        </div>
-        
-        
-  </div>
-    </div>
-  </div>
-
-
-
-  <div class="home-aboutus-wrapper detail-page">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-12 head">
-                  <h2>Two Row Multi-Boring Machine</h2>
-              </div>
-              <!-- <div class="paragraph">
-                  <p>Minimum trouble; maximum dependability comprehensive technical service</p>
-              </div> -->
-          </div>
+         </div>
       </div>
-  </div>
-
-  <div class="home-aboutus-wrapper detail-page">
-      <div class="container">
-        <div class="row">
-        <div class="col-md-12 text-center">
-          <div class="col-md-6 ">
+   </div>
+</div>
+<div class="home-aboutus-wrapper detail-page">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12 head">
+            <h2>Two Row Multi-Boring Machine</h2>
+         </div>
+         <!-- <div class="paragraph">
+            <p>Minimum trouble; maximum dependability comprehensive technical service</p>
+            </div> -->
+      </div>
+   </div>
+</div>
+<div class="home-aboutus-wrapper detail-page">
+<div class="container">
+   <div class="row">
+      <div class="col-md-12 text-center">
+         <div class="col-md-6 ">
             <div class="about-text">
-              
-            <img src="images/Multi-Boring-42.png" alt=""> </div>
-        <a href="contact.html" class="btn">Contact Us</a> <a href="tel:919723468755" class="btn">Call Now</a>
-          </div>
-          <div class="col-md-6">
-            <div class="about-text">
-            <!-- <h3>Reciprocating Beam Saw Machine</h3><hr/> -->
-              
-            <table>
-    <tr>
-      <th>MODEL</th>
-      <th>UMB 21</th>
-      
-    </tr>
-    <tr>
-      <td><b>Distance between the bit</b> </td>
-      <td>3280 mm</td>
-      
-    </tr>
-    <tr>
-      <td><b>Horizontal head</b> </td>
-      <td>1</td>
-      
-    </tr>
-    <tr>
-      <td><b>Vertical head</b> </td>
-      <td>1</td>
-      
-    </tr>
-    <tr>
-      <td><b>Tilting Head</b> </td>
-      <td>1</td>
-      
-    </tr>
-    <tr>
-      <td><b>The quantity of spindle</b></td>
-      <td>42</td>
-      
-    </tr>
-    <tr>
-      <td><b>Maximum boring diameter</b></td>
-      <td>35mm</td>
-      
-    </tr>
-    <tr>
-      <td><b>Maximum panel thickness</b></td>
-      <td>60mm</td>
-      
-    </tr>
-    <tr>
-      <td><b>Maximum panel width</b></td>
-      <td>2400mm</td>
-      
-    </tr>
-    <tr>
-      <td><b>Minimum panel width</b></td>
-      <td>200mm</td>
-      
-    </tr>
-    <tr>
-      <td><b>Total power</b></td>
-      <td>3kw</td>
-      
-    </tr>
-  </table>
-
-              
+               <img src="images/Multi-Boring-42.png" alt=""> 
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="home-aboutus-wrapper detail-page">
-      <div class="container">
-        <div class="row">
-      <div class="col-md-6 text-center">
+            <a href="contact.html" class="btn">Contact Us</a> <a href="tel:919723468755" class="btn">Call Now</a>
+         </div>
+         <div class="col-md-6">
             <div class="about-text">
-        
-        <img src="images/UMB_42_1.jpg" alt=""> </div>
-        <a href="contact.html" class="btn">Contact Us</a> <a href="tel:919723468755" class="btn">Call Now</a>
-          </div>
-          <div class="col-md-6">
-            <div class="about-text">
-        <h3>Tilting Drilling Head </h3><hr/>
-          <ul class="list">
-            <li>Can do accurate hole on surface as well as on edge of board.</li>
-            <li>Tilting drilling head can move on 90</li>
-          </ul>            
+               <!-- <h3>Reciprocating Beam Saw Machine</h3><hr/> -->
+               <table>
+                  <tr>
+                     <th>MODEL</th>
+                     <th>UMB 21</th>
+                  </tr>
+                  <tr>
+                     <td><b>Distance between the bit</b> </td>
+                     <td>3280 mm</td>
+                  </tr>
+                  <tr>
+                     <td><b>Horizontal head</b> </td>
+                     <td>1</td>
+                  </tr>
+                  <tr>
+                     <td><b>Vertical head</b> </td>
+                     <td>1</td>
+                  </tr>
+                  <tr>
+                     <td><b>Tilting Head</b> </td>
+                     <td>1</td>
+                  </tr>
+                  <tr>
+                     <td><b>The quantity of spindle</b></td>
+                     <td>42</td>
+                  </tr>
+                  <tr>
+                     <td><b>Maximum boring diameter</b></td>
+                     <td>35mm</td>
+                  </tr>
+                  <tr>
+                     <td><b>Maximum panel thickness</b></td>
+                     <td>60mm</td>
+                  </tr>
+                  <tr>
+                     <td><b>Maximum panel width</b></td>
+                     <td>2400mm</td>
+                  </tr>
+                  <tr>
+                     <td><b>Minimum panel width</b></td>
+                     <td>200mm</td>
+                  </tr>
+                  <tr>
+                     <td><b>Total power</b></td>
+                     <td>3kw</td>
+                  </tr>
+               </table>
             </div>
-          </div>
-        </div>
+         </div>
       </div>
-    </div>
-
-    <div class="home-aboutus-wrapper detail-page">
-      <div class="container">
-        <div class="row">
-  
-          <div class="col-md-6">
+   </div>
+</div>
+<div class="home-aboutus-wrapper detail-page">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6 text-center">
             <div class="about-text">
-        <h3>Vertical Boring Head </h3><hr/>
-          <ul class="list">
-            <li>21 drilling bits at 32mm distance.</li>
-          </ul>            
+               <img src="images/UMB_42_1.jpg" alt=""> 
             </div>
-          </div>
-          <div class="col-md-6 text-center">
+            <a href="contact.html" class="btn">Contact Us</a> <a href="tel:919723468755" class="btn">Call Now</a>
+         </div>
+         <div class="col-md-6">
             <div class="about-text">
-        
-        <img src="images/UMB_42_2.jpg" alt=""> </div>
-        <a href="contact.html" class="btn">Contact Us</a> <a href="tel:919723468755" class="btn">Call Now</a>
-          </div>
-        </div>
+               <h3>Tilting Drilling Head </h3>
+               <hr/>
+               <ul class="list">
+                  <li>Can do accurate hole on surface as well as on edge of board.</li>
+                  <li>Tilting drilling head can move on 90</li>
+               </ul>
+            </div>
+         </div>
       </div>
-    </div>
-
-    <?php include('footer.php'); ?>
+   </div>
+</div>
+<div class="home-aboutus-wrapper detail-page">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="about-text">
+               <h3>Vertical Boring Head </h3>
+               <hr/>
+               <ul class="list">
+                  <li>21 drilling bits at 32mm distance.</li>
+               </ul>
+            </div>
+         </div>
+         <div class="col-md-6 text-center">
+            <div class="about-text">
+               <img src="images/UMB_42_2.jpg" alt=""> 
+            </div>
+            <a href="contact.html" class="btn">Contact Us</a> <a href="tel:919723468755" class="btn">Call Now</a>
+         </div>
+      </div>
+   </div>
+</div>
+</div>
+<?php include('footer.php'); ?>
