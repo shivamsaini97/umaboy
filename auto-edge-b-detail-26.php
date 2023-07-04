e06bff2f015a497302f3e5ffa343ee4c67a1902f
Shivam Saini <shivam.cotginanalytics@gmail.com>
2023-07-03 22:15:13 -0700

    header

---
+++ auto-edge-b-detail-26.php
@@ -0,0 +1,162 @@
+<?php include('header.php'); ?>
+
+<div class="home-slider-area">
+  <div id="welcome-slide-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+   
+    <div class="carousel-inner" role="listbox"> 
+    
+      <div class="item active">
+        <div class="single-slide-item slide-5">
+          <div class="single-slide-item-table">
+            <div class="single-slide-item-table-cell">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-12">
+                    <h2 style="text-transform:capitalize">Auto Edge Bander Expert 2600</span></h2>
+                   
+                   <!-- <a class="slide-btn" href="javascript:void(0)">Contact Us</a>--></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
+ </div>
+  </div>
+</div>
+
+
+
+<div class="home-aboutus-wrapper detail-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 head">
+                <h2>Cornor Rounding
+                with affordable prize</h2>
+            </div>
+            <div class="paragraph">
+                <p>Expert 2600 Edge Banding Machine stands for Minimum Investment to get Maximum Output. The expert 2600 series is your guarantee for absolute quality, flexibility and productivity. The smart element of the machine is exclusive price with attractive result. Expert 2600 Edge banding machine has ergonomic structure and the fastest, shortest and easy to be used system and designed for PVC, Foil, Lining Melamine and ABS applications thickness of which is from 0.4 mm up to 3mm for workshops, factories and out sources with its highest quality</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="home-aboutus-wrapper detail-page">
+    <div class="container">
+      <div class="row">
+       <div class="col-md-6 text-center">
+          <div class="about-text">
+          
+          <img src="images/2600.png" alt=""> </div>
+      <a href="contact.php" class="btn">Contact Us</a> <a href="tel:919723468755" class="btn">Call Now</a>
+        </div>
+        <div class="col-md-6">
+          <div class="about-text">
+          <h3>Auto Edge Bander Expert 2600</h3><hr/>
+            
+            <table>
+    <tr>
+      <th>MODEL</th>
+      <th>EXPERT 2600</th>
+      
+    </tr>
+    <tr>
+      <td><b>Electric leve</b> </td>
+      <td>415-220/50 hz</td>
+      
+    </tr>
+    <tr>
+      <td><b>Nominal current</b> </td>
+      <td>18 A</td>
+      
+    </tr>
+    <tr>
+      <td><b>Edge material thickness</b> </td>
+      <td>0.4-2mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Board thickness</b> </td>
+      <td>10-50mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Speed  </b> </td>
+      <td>12 m/min</td>
+      
+    </tr>
+    <tr>
+      <td><b>Total electrical power</b></td>
+      <td>4kw</td>
+      
+    </tr>
+    <tr>
+      <td><b>Total air pressure</b></td>
+      <td>6 Bar</td>
+      
+    </tr>
+    <tr>
+      <td><b>Total air usage</b></td>
+      <td>160 Lt/min.</td>
+      
+    </tr>
+    <tr>
+      <td><b>Buffing motor</b></td>
+      <td>2x0.18kw/1310rpm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Glue boiler engine</b></td>
+      <td>0.18kwx900rpm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Trimming</b></td>
+      <td>2x0.75kw/18000rpm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Pallet Motors</b></td>
+      <td>1.1kw/900rpm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Machine length</b></td>
+      <td>2500mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Machine width</b></td>
+      <td>800mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>Machine height</b></td>
+      <td>1500mm</td>
+      
+    </tr>
+    <tr>
+      <td><b>PLC</b></td>
+      <td>User friendly touch screen</td>
+      
+    </tr>
+  </table>
+  <!--<strong>Type</strong>: &nbsp;&nbsp;AD112 - 22FBA <br/><br/>
+  <strong>Color</strong>: &nbsp;&nbsp;White, Green, Red, Yellow, Blue<br/><br/>
+  <strong>Parameter</strong>: &nbsp;&nbsp;0-50A | 0-100A | 0-200A | 0-500A <br/><br/>
+  <strong>Outline Dimension</strong>:&nbsp;&nbsp;<img src="images/d1.jpg"><br/>-->
+  </p>
+  <p>
+    Note: Technical specifications may be changed without any prior notice. Product images are just for the reference.
+  </p>
+            
+           </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <?php include('footer.php'); ?>
